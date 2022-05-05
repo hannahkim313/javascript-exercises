3aa2c18b6650d834312685b8d779fdb5cab2f912
Hannah Kim <devhkim13@gmail.com>
2022-05-04 22:14:54 -0700

    Resolve second spec test by redefining removeFromArray function
    - Add rest parameters to account for multiple optional parameters
    
    - Compare each element of params to the elements in array and remove matching values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,18 @@
-const removeFromArray = function(array, n) {
-    for (let i = 0; i < array.length - 1; i++) {
-        if (array[i] === n) {
-            array.splice(i, 1);
-            return array;
-        } else {
-            continue;
+const removeFromArray = function(array, a, ...params) {
+    // Create single array for comparison
+    params.unshift(a);
+
+    for (let i = 0; i <= params.length - 1; i++) {
+        for (let j = 0; j < array.length - 1; j++) {
+            if (array[j] === params[i]) {
+                array.splice(j, 1);
+                // break;
+            } else {
+                continue;
+            }
         }
     }
+    return array;
 };
 
 // Do not edit below this line
