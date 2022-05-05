1ffd9e02ba2653443984279eabd1c0189a3ae229
Hannah Kim <devhkim13@gmail.com>
2022-05-05 16:05:10 -0700

    Resolve spec tests by defining sum function
    - Return sum of numbers by looping through array and adding the value at each index to sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,14 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+    let sum = 0;
+
+	for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    
+    return sum;
 };
 
 const multiply = function() {
