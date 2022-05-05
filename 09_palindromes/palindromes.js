25953a2b836d17348ed6693c92d2e59512cc824c
Hannah Kim <devhkim13@gmail.com>
2022-05-05 16:46:34 -0700

    Resolve first spec test by defining palindromes function
    - Convert string parameter into an array of characters to loop and push each element into charsReversed in reverse order
    
    - Return false as soon as a character at the same index of each array doesn't match

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const chars = string.split("");
+    const charsReversed = [];
+    
+    for (let i = chars.length - 1; i >= 0; i--) {
+        charsReversed.push(chars[i]);
+    }
 
+    for (let i = 0; i < chars.length; i++) {
+        if (chars[i] !== charsReversed [i]) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
