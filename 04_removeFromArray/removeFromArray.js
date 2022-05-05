960a4af325c7d0b1b0e294eb9491434bdc6ecd8d
Hannah Kim <devhkim13@gmail.com>
2022-05-04 19:14:07 -0700

    Resolve first spec test by defining a function expression
    - Loop through array and remove elements at index i that strictly equals n

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, n) {
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] === n) {
+            array.splice(i, 1);
+            return array;
+        } else {
+            continue;
+        }
+    }
 };
 
 // Do not edit below this line
