09f926e0cf933b9e6d8c165d1dde9a85f2236c8d
Hannah Kim <devhkim13@gmail.com>
2022-05-05 16:57:47 -0700

    Resolve second spec test by redefining palindromes function
    - Remove punctuation from string parameter

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,15 @@
 const palindromes = function (string) {
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+                .replace(/\s{2,}/g," ");
     const chars = string.split("");
     const charsReversed = [];
-    
+
     for (let i = chars.length - 1; i >= 0; i--) {
         charsReversed.push(chars[i]);
     }
 
     for (let i = 0; i < chars.length; i++) {
-        if (chars[i] !== charsReversed [i]) return false;
+        if (chars[i] !== charsReversed[i]) return false;
     }
 
     return true;
