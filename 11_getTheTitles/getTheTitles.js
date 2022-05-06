882bdc508d800eef1e4d99d1ecb511898b66da5b
Hannah Kim <devhkim13@gmail.com>
2022-05-06 09:17:30 -0700

    Resolve spec test by defining getTheTitles function
    - Retrieve and push title of each object in array a to array titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(a) {
+    const titles = [];
 
+    for (let i = 0; i < a.length; i++) {
+        titles.push(a[i].title);
+    }
+
+    return titles;
 };
 
 // Do not edit below this line
