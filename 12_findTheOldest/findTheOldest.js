318a0a21cee8d163ee0b37d667dac0ed3a5c9f71
Hannah Kim <devhkim13@gmail.com>
2022-05-06 12:09:26 -0700

    Define findTheOldest function that returns the object with the oldest person
    - Spec tests return errors

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
+    const oldestPerson = a.reduce((prevPerson, currentPerson) => {
+        if (prevPerson.yearOfDeath === undefined) {
+            const today = new Date();
+            prevPerson.yearOfDeath = today.getFullYear();
+        }
 
+        let oldestAge = prevPerson.yearOfDeath - prevPerson.yearOfBirth;
+        let currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+
+        return (currentAge > oldestAge) ? currentPerson : prevPerson;
+    });
 };
 
 // Do not edit below this line
