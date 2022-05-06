644b291c2317ba422d7a15ef664473fdf30be3be
Hannah Kim <devhkim13@gmail.com>
2022-05-06 14:03:02 -0700

    Resolve first spec test by defining caesar function
    - Return new string after converting chars array filled with unicode values plus num of string parameter

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,12 @@
-const caesar = function() {
+const caesar = function(string, num) {
+    const chars = [];
+    
+    for (let i = 0; i < string.length; i++) {
+        chars.push(string.charCodeAt(i));
+        chars[i] = chars[i] + num;
+    }
 
+    return String.fromCharCode(chars);
 };
 
 // Do not edit below this line
