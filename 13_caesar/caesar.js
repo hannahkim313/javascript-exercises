fa877cb15173e1acf8b230c03830d024b38310aa
Hannah Kim <devhkim13@gmail.com>
2022-05-06 14:25:12 -0700

    Resolve second spect test by redefining caesar function
    - Change parameter for fromCharCode() from an array to the value at each index of chars array
    
    - Fix return value by appending the letter of the new unicode value to newString

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,12 +1,14 @@
 const caesar = function(string, num) {
     const chars = [];
-    
+    let newString = "";
+
     for (let i = 0; i < string.length; i++) {
         chars.push(string.charCodeAt(i));
         chars[i] = chars[i] + num;
+        newString += String.fromCharCode(chars[i]);
     }
 
-    return String.fromCharCode(chars);
+    return newString;
 };
 
 // Do not edit below this line
