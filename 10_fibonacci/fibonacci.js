bf1a629cd698d2e0469abd3fa266facb08494461
Hannah Kim <devhkim13@gmail.com>
2022-05-05 21:36:06 -0700

    Resolve first spec test by defining fibonacci function
    - Define and fill numbers array with first 25 fibonacci numbers
    
    - Account for first two indices in numbers array since negative indices are inaccessible when calculating the sum of the two preceding numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    const numbers = [];
 
+    for (let i = 0; i < 25; i++) {
+        if (i === 0) {
+            numbers.push(1);
+        } else if (i === 1) {
+            numbers.push(0 + numbers[i - 1]);
+        } else {
+            numbers.push(numbers[i - 2] + numbers[i - 1]);
+        }
+    }
+    return numbers[a - 1];
 };
 
-// Do not edit below this line
+// Do not edit below this
 module.exports = fibonacci;
