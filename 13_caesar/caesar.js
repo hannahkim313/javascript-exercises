72608eecac5cef0719ce5349e16d34cb8885aad6
Hannah Kim <devhkim13@gmail.com>
2022-05-07 20:14:31 -0700

    Resolve third spec test by redefining caesar function
    - Change parameter name from 'num' to 'shift' for better clarity
    
    - Define and fill arrays with unicode values of either all lowercase or uppercase letters to cycle through when performing the shift

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,14 +1,30 @@
-const caesar = function(string, num) {
+const caesar = function(string, shift) {
     const chars = [];
+    const upperUni = [];
+    const lowerUni = [];
     let newString = "";
+
+    for (let i = 65; i < 91; i++) upperUni.push(i);
+    for (let i = 97; i < 123; i++) lowerUni.push(i);
 
     for (let i = 0; i < string.length; i++) {
         chars.push(string.charCodeAt(i));
-        chars[i] = chars[i] + num;
+
+        if (chars[i] >= 65 && chars[i] <= 90) {
+            chars[i] = (chars[i] + shift > 90) ?
+                upperUni[(shift - (upperUni[25] - chars[i])) - 1] :
+                chars[i] = chars[i] + shift;
+        }
+
+        if (chars[i] >= 97 && chars[i] <= 122) {
+            chars[i] = (chars[i] + shift > 122) ?
+                lowerUni[(shift - (lowerUni[25] - chars[i])) - 1] :
+                chars[i] = chars[i] + shift;
+        }
+
         newString += String.fromCharCode(chars[i]);
     }
-
-    return newString;
+    return newString
 };
 
 // Do not edit below this line
